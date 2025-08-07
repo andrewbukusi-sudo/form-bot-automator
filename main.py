869c877b151e929e05330636d0488dac0334f962
andrewbukusi-sudo <andrewbukusi@gmail.com>
2025-08-07 14:44:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask
-from scheduler import submit_form
-import random
+from scheduler import run_scheduler
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    try:
-        submit_form()
-        return "✅ Submitted 1 form successfully!"
-    except Exception as e:
-        return f"❌ Error: {e}"
+    return '✅ Form submission bot is running!'
+
+@app.route('/run')
+def run():
+    run_scheduler()
+    return '✅ Scheduler executed successfully.'
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, port=5000)
